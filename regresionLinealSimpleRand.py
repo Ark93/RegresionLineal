ca5381274b4d94d44d70ced4dd775d012c325788
Santos Bañuelos <santos.banuelos93@gmail.com>
2016-01-05 21:39:53 -0600

    Change name of python file to regresionLinealSimpleRand.py add RSS to plot

---
+++ regresionLinealSimpleRand.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# function to elevate data to 10^3 in cientific notation
+# because the data is originaly between 0 and 1
+def e3(x):
+    return x*(10**3)
+
+#function to plot data
+def plot_data(data):
+    plt.plot(data[0],data[1],'ro')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Regresion Lineal Simple')
+    plt.show()
+
+def plot_prediction(data,prediction, rss):
+    plt.plot(data[0],data [1], 'ro', data[0], prediction)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    textRSS = "RSS =", rss
+
+    plt.title('Regresion Lineal Simple')
+    plt.annotate( textRSS ,xy = (data[0].mean(), data[1].mean() ), xytext=(data[0].mean()-100, data[1].mean()+40))
+    plt.show()
+
+#function to get coefficient w1
+def getW1(data):
+    return ( ((data[0] - data[0].mean()) * (data[1] - data[1].mean())  ).sum() / ((data[0] - data[0].mean())**2).sum() )
+
+#funciton to get coefficient w0
+def getW0(data,w1):
+    return data[1].mean() - (w1*data[0].mean())
+
+#function to predict values of linear regression
+def predict(feature,w):
+    y = np.zeros(len(feature))
+    for i in range(0,len(y)):
+        y[i] = w[0] + w[1]*feature[i]
+    return y
+
+def getRSS(data,prediction):
+    rss = 0
+    for i in range(0,len(data)):
+        rss += (data[i] - prediction[i])**2
+    return rss
+
+# creation of 30 points of data, with x and y values
+data = np.random.rand(2,50)
+w = np.zeros(2)
+#data to 10^3
+data= e3(data)
+
+#caption of coefficients
+w[1] = getW1(data)
+w[0] = getW0(data,w[1])
+
+print w
+
+y = predict(data[0],w)
+
+rss = getRSS(data[1],y)
+plot_prediction(data,y, rss)
+
